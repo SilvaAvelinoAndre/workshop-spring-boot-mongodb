586a38ae69eb09b62c3774ab2c3e1658fe2b0d27
Andre Avelino da Silva <andreavelinosilva@hotmail.com>
2022-02-28 19:24:13 -0300

    Referenciando os posts do usuário ok

--- src/main/java/com/aastechnology/workshopmongo/config/Instantiation.java
+++ src/main/java/com/aastechnology/workshopmongo/config/Instantiation.java
@@ -1,17 +1,17 @@
 package com.aastechnology.workshopmongo.config;
 
-import java.text.SimpleDateFormat;
 import java.util.Arrays;
-import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+<<<<<<< HEAD
 import com.aastechnology.workshopmongo.DTO.AuthorDTO;
 import com.aastechnology.workshopmongo.domain.Post;
+=======
+>>>>>>> parent of 1a5be9e (Criando entity Post com User aninhado)
 import com.aastechnology.workshopmongo.domain.User;
-import com.aastechnology.workshopmongo.repository.PostRepository;
 import com.aastechnology.workshopmongo.repository.UserRepository;
 
 @Configuration
@@ -20,22 +20,18 @@ public class Instantiation implements CommandLineRunner{
 	@Autowired
 	private UserRepository userRepository;
 	
-	@Autowired
-	private PostRepository postRepository;
+	
 	
 	@Override
 	public void run(String... args) throws Exception {
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
-		
 		userRepository.deleteAll();
-		postRepository.deleteAll();
 		
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+<<<<<<< HEAD
 		
 		
 		Post post1 = new Post(null,  sdf.parse("23/03/2018"), "Partiu viagem", "Vou viajar para São Paulo, Abraços!", new AuthorDTO(maria));
@@ -43,6 +39,8 @@ public class Instantiation implements CommandLineRunner{
 		
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
+=======
+>>>>>>> parent of 1a5be9e (Criando entity Post com User aninhado)
 		
 		maria.getPosts().addAll(Arrays.asList(post1, post2));
 		userRepository.save(maria);
