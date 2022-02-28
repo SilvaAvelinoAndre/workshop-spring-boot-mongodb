dda56c56b19f5ef386925ed001d20b6a59f020ee
Andre Avelino da Silva <andreavelinosilva@hotmail.com>
2022-02-28 12:48:44 -0300

    Inserção de usuário com POST

--- src/main/java/com/aastechnology/workshopmongo/services/UserService.java
+++ src/main/java/com/aastechnology/workshopmongo/services/UserService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.aastechnology.workshopmongo.DTO.UserDTO;
 import com.aastechnology.workshopmongo.domain.User;
 import com.aastechnology.workshopmongo.repository.UserRepository;
 import com.aastechnology.workshopmongo.services.exception.ObjectNotFoundException;
@@ -25,5 +26,13 @@ public class UserService {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 		}
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	public User fromDTO(UserDTO objDTO) {
+		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
+	}
+	
+	
 
 }
