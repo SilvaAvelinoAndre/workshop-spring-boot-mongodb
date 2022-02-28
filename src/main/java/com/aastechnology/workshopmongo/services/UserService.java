a1b759047376299778ef36877b243375eb0e4269
Andre Avelino da Silva <andreavelinosilva@hotmail.com>
2022-02-28 13:21:12 -0300

    Atualização de usuário com PUT

--- src/main/java/com/aastechnology/workshopmongo/services/UserService.java
+++ src/main/java/com/aastechnology/workshopmongo/services/UserService.java
@@ -13,26 +13,42 @@ import com.aastechnology.workshopmongo.services.exception.ObjectNotFoundExceptio
 
 @Service
 public class UserService {
-	
-	@Autowired // injeção de dependencia 
+
+	@Autowired // injeção de dependencia
 	private UserRepository repo;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repo.findAll();
-		
+
 	}
-	
+
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
-		}
+	}
+
 	public User insert(User obj) {
 		return repo.insert(obj);
 	}
+
+	public void delete(String id) {
+		findById(id);
+		repo.deleteById(id);
+		
+	}
+	public User update(User obj) {
+		User newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+		}
+
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+	}
+
 	public User fromDTO(UserDTO objDTO) {
 		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
 	}
-	
-	
 
 }
