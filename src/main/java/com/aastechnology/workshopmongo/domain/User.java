4a5f4cd456a0df760ebe56b2a3e19f8a31d0ec06
Andre Avelino da Silva <andreavelinosilva@hotmail.com>
2022-02-28 16:03:56 -0300

    Projeção dos dados do autor com DTO

--- src/main/java/com/aastechnology/workshopmongo/domain/User.java
+++ src/main/java/com/aastechnology/workshopmongo/domain/User.java
@@ -1,9 +1,12 @@
 package com.aastechnology.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document // anotation indica que a classe é uma coleção do mongodb
@@ -16,6 +19,9 @@ public class User implements Serializable {
 	private String id;
 	private String name;
 	private String email;
+	
+	@DBRef(lazy = true) // Anotation referencia a lista com o mongodb lazy=true, garante que os post só seram carregar se for acessados, se acessar os usuarios os posts não serão carregado(para não gerar trafego desnecessarios)
+	private List<Post> posts = new ArrayList<>();
 
 	public User() {
 	}
@@ -50,6 +56,13 @@ public class User implements Serializable {
 	public void setEmail(String email) {
 		this.email = email;
 	}
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
+	}
 
 	@Override
 	public int hashCode() {
@@ -67,5 +80,7 @@ public class User implements Serializable {
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
 	}
+
+	
 
 }
